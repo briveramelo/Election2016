ea5e85f4cf8b2fd62ef9c969a0faafb94f9e2819
BRANDON RIVERA-MELO <u1089885@ad.utah.edu>
2016-11-09 19:15:35 -0700

    Initial code

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Rigidbody myBody;
+    [SerializeField, Range(500,5000)] float attractionForce;
+
+    // Use this for initialization
+    void Start() {
+        attractionForce = 500;
+    }
+
+    Attraction myAttractionState = Attraction.None;
+    enum Attraction {
+        In=0,
+        Out=1,
+        None=2
+    }
+
+    struct GrenadeStats {
+        public Vector3 origin;
+        public float radius;
+        public GrenadeStats(Vector3 origin, float radius) {
+            this.origin = origin;
+            this.radius = radius;
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ToggleState();
+        }
+    }
+
+    void OnTriggerStay(Collider col) {
+        if (col.GetComponent<Ball>()) {
+            switch (myAttractionState) {
+                case Attraction.In:
+                    Magnetize(col.GetComponent<Rigidbody>());
+                    break;
+                case Attraction.Out:
+                    Repel(col.GetComponent<Rigidbody>());
+                    break;
+            }            
+        }
+    }
+
+    void Magnetize(Rigidbody rigBod) {
+        Vector3 otherPos = rigBod.transform.position;
+        Vector3 pointAtMe = transform.position - otherPos;
+
+        rigBod.AddForce(attractionForce * pointAtMe);
+    }
+
+    void Repel(Rigidbody rigBod) {
+        Vector3 otherPos = rigBod.transform.position;
+        Vector3 pointAway = otherPos - transform.position;
+        float repulsionForce = attractionForce *  1/ Vector3.Distance(otherPos, transform.position);
+        rigBod.AddForce(repulsionForce * pointAway);
+    }
+
+    
+    public void ToggleState() {
+        myAttractionState++;
+        if ((int)myAttractionState >= 3) {
+            myAttractionState = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Cannon : MonoBehaviour {
+
+    [SerializeField] GameObject cBall;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            LaunchBall(KeyCode.UpArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            LaunchBall(KeyCode.LeftArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            LaunchBall(KeyCode.RightArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            LaunchBall(KeyCode.DownArrow);
+        }
+    }
+
+    float launchForce = 10000000;
+    void LaunchBall(KeyCode direction) {
+        Vector3 launchDirection = Vector3.zero;
+        Vector3 spawnPosition = Vector3.zero;
+        switch (direction) {
+            case KeyCode.UpArrow:
+                launchDirection = Vector3.up;
+                spawnPosition = -Vector3.up * 20f;
+                break;
+            case KeyCode.DownArrow:
+                launchDirection = Vector3.down;
+                spawnPosition = -Vector3.down * 20f;
+                break;
+            case KeyCode.LeftArrow:
+                launchDirection = Vector3.left;
+                spawnPosition = -Vector3.left * 40f;
+                break;
+            case KeyCode.RightArrow:
+                launchDirection = Vector3.right;
+                spawnPosition = -Vector3.right * 40f;
+                break;
+        }
+
+        GameObject ball = Instantiate(cBall, spawnPosition, Quaternion.identity) as GameObject;
+        ball.GetComponent<Rigidbody>().AddForce(launchDirection * launchForce);
+        Destroy(ball, 10f);
+    }
+
+
+}
